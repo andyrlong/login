4f0810614c8517f99b638a5fee229b7d72dc991f
Andrew Long <54878469+andyrlong@users.noreply.github.com>
2023-06-09 21:22:47 -0400

    ogin.py

--- login.py
+++ login.py
@@ -8,12 +8,12 @@ password_input = input("Enter your password: ")
 
 if username_input != username and password_input != password:
     print("Incorrect username and password. Try again.")
-    time.sleep(0.5)
+    time.sleep(2)
 elif username_input == username and password_input != password:
     print("Incorrect password. Try again.")
-    time.sleep(0.5)
+    time.sleep(2)
 elif username_input != username and password_input == password:
     print("Incorrect password. Try again.")
-    time.sleep(0.5)
+    time.sleep(2)
 else: 
     print("Access granted.")
