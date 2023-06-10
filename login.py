af769fa723f04499231ea42339e35aa2f4356e8c
Andrew Long <54878469+andyrlong@users.noreply.github.com>
2023-06-09 21:24:42 -0400

    login.py

--- login.py
+++ login.py
@@ -1,11 +1,15 @@
 import time
 
+# Store username and password
 username = 'andyrlong'
 password = 'password'
 
+# Prompts the user to enter a username and password
 username_input = input("Enter your username: ")
 password_input = input("Enter your password: ")
 
+# If the username and password both match, the user is granted access
+# Otherwise, the user must wait two seconds before their next attempt
 if username_input != username and password_input != password:
     print("Incorrect username and password. Try again.")
     time.sleep(2)
